9523203645aa426016140af2cb05e60e36865989
Rômulo Fortaleza <romulofortaleza_@hotmail.com>
2018-10-09 13:25:13 -0300

    Models Validations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :favoritable, polymorphic: true
   belongs_to :user
-end
+  validates :user_id, uniqueness: { scope: [:favoritable_type, :favoritable_id], message: "can favorite only one time per resource" }
+ end
